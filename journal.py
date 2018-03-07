fa9aab1ea5777b51767f2d9369748ee480c9ded0
Jesse Tyner-Bryan <jesse.tynerbryan@gmail.com>
2018-03-07 13:12:10 -0500

    able to persist new entries and read them via command line

--- journal.py
+++ journal.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+from collections import OrderedDict
 import datetime
 
 from peewee import *
@@ -20,15 +21,38 @@ def initialize():
 
 def menu_loop():
     """Show the Menu"""
+    choice = None
+
+    while choice != 'q':
+        print("Enter 'q' to quit")
+        for key, value in menu.items():
+            print(f'{key}) {value.__doc__}')
+        choice = input('Action: ').lower().strip()
+
+        if choice in menu:
+            menu[choice]()
+        else:
+            continue
 
 def add_entry():
     """Create New Entry"""
+    entry = input("Write your entry here: ")
+
+    Entry.create(content=entry)
 
 def view_entries():
     """View all entries"""
+    entries = Entry.select()
+    for entry in entries:
+        print(entry.content)
 
 def delete_entry(entry):
     """Delete an Entry"""
+
+menu = OrderedDict([
+    ('a', add_entry),
+    ('v', view_entries)
+])
 
 if __name__ == '__main__':
     initialize()
